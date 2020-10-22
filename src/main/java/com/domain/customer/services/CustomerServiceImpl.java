afc253f96005f249e0c09b9c1629520e75119642
JoseLuisSR <jose.luis.sastoque.rey@gmail.com>
2020-10-21 22:28:26 -0500

    Update architecture diagram & add logger.

--- src/main/java/com/domain/customer/services/CustomerServiceImpl.java
+++ src/main/java/com/domain/customer/services/CustomerServiceImpl.java
@@ -3,6 +3,8 @@ package com.domain.customer.services;
 import com.domain.customer.controllers.CustomerService;
 import com.domain.customer.entities.Customer;
 import com.domain.customer.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,15 @@ import java.util.UUID;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     CustomerRepository customerRepository;
 
     @Override
     public String createCustomer(Customer customer){
 
+        logger.info("Create Customer");
         customer.setId(UUID.randomUUID().toString());
         customerRepository.save(customer);
         return customer.getId().toString();
@@ -24,6 +29,8 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Iterable<Customer> readCustomers() {
+
+        logger.info("Read Customer");
         return customerRepository.findAll();
     }
 
