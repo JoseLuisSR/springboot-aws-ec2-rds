321d86e6ef703ed1bbc0931040894cc6ef28bdc5
JoseLuisSR <jose.luis.sastoque.rey@gmail.com>
2022-07-07 20:38:17 -0500

    Remove warnings and unused import classes.

--- src/main/java/com/domain/customer/services/CustomerService.java
+++ src/main/java/com/domain/customer/services/CustomerService.java
@@ -2,8 +2,6 @@ package com.domain.customer.services;
 
 import com.domain.customer.entities.Customer;
 import com.domain.customer.controllers.out.CustomerRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -13,10 +11,6 @@ import java.util.UUID;
 
 @Service
 public class CustomerService {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    private static final String EMPTY = "";
 
     @Autowired
     CustomerRepository customerRepository;
@@ -46,5 +40,4 @@ public class CustomerService {
 
         customerRepository.deleteById(id);
     }
-
 }
